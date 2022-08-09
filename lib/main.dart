8cbab9694ab47034834ed9225af92ecaafc139f4
dhzdhd <ragas5432100@gmail.com>
2022-08-09 19:51:36 +0530

    Add bitsdojo package and implement themed titlebar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:bitsdojo_window/bitsdojo_window.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:get/get.dart';
@@ -10,8 +12,18 @@ import 'Pages/Settings/settings_page.dart';
 main() async {
   await GetStorage.init();
   WidgetsFlutterBinding.ensureInitialized();
-  await Future.delayed(const Duration(milliseconds: 300)); // HACK: fix for https://github.com/flutter/flutter/issues/101007
+  await Future.delayed(const Duration(
+      milliseconds:
+          300)); // HACK: fix for https://github.com/flutter/flutter/issues/101007
   runApp(const MyApp());
+
+  if ([TargetPlatform.windows, TargetPlatform.linux, TargetPlatform.macOS]
+      .contains(defaultTargetPlatform)) {
+    doWhenWindowReady(() {
+      appWindow.alignment = Alignment.center;
+      appWindow.show();
+    });
+  }
 }
 
 const List<Color> colorOptions = [
@@ -36,8 +48,8 @@ const List<String> colorText = <String>[
   "Red",
 ];
 
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -64,15 +76,16 @@ class _MyAppState extends State<MyApp> {
   }
 
   Future<void> initNotifs() async {
-    FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-    const AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings('launcher_icon');
-    const InitializationSettings initializationSettings = InitializationSettings(
+    FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+        FlutterLocalNotificationsPlugin();
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('launcher_icon');
+    const InitializationSettings initializationSettings =
+        InitializationSettings(
       android: initializationSettingsAndroid,
     );
-    await flutterLocalNotificationsPlugin.initialize(
-        initializationSettings,
-        onSelectNotification: selectNotification
-    );
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: selectNotification);
   }
 
   void selectNotification(String? payload) async {
@@ -80,7 +93,6 @@ class _MyAppState extends State<MyApp> {
       debugPrint('notification payload: $payload');
     }
   }
-
 
   void changeDarModeEnabled(bool newVal) {
     setState(() {
@@ -107,39 +119,120 @@ class _MyAppState extends State<MyApp> {
         useMaterial3: true,
         colorSchemeSeed: colorOptions[colorSelected],
         brightness: Brightness.light,
-        textTheme: GoogleFonts.poppinsTextTheme(ThemeData
-            .light()
-            .textTheme),
+        textTheme: GoogleFonts.poppinsTextTheme(ThemeData.light().textTheme),
       ),
       darkTheme: ThemeData(
         useMaterial3: true,
         colorSchemeSeed: colorOptions[colorSelected],
         brightness: Brightness.dark,
-        textTheme: GoogleFonts.poppinsTextTheme(ThemeData
-            .dark()
-            .textTheme),
+        textTheme: GoogleFonts.poppinsTextTheme(ThemeData.dark().textTheme),
       ),
       themeMode: ThemeMode.light,
-      home: SafeArea(
-          top: true,
-          child: Main(
-            title: 'Pomo Focus',
-            darkModeEnabled: darkModeEnabled,
-            changeDarModeEnabled: changeDarModeEnabled,
-            changeColorSelected: changeColorSelected,
-            colorSelected: colorSelected,
-          )),
+      home: Column(
+        children: [
+          if ([
+            TargetPlatform.windows,
+            TargetPlatform.linux,
+            TargetPlatform.macOS
+          ].contains(defaultTargetPlatform))
+            WindowTitleBarBox(
+              child: Container(
+                color: darkModeEnabled ? Colors.white : Colors.black,
+                child: Row(children: [
+                  Expanded(
+                      child: MoveWindow(
+                          child: Center(
+                              child: Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: DefaultTextStyle(
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: darkModeEnabled ? Colors.black : Colors.white,
+                          fontWeight: FontWeight.normal,
+                        ),
+                        child: const Text(
+                          'Pomo Focus',
+                        ),
+                      ),
+                    ),
+                  )))),
+                  WindowButtons(
+                    isDarkMode: darkModeEnabled,
+                  ),
+                ]),
+              ),
+            ),
+          Expanded(
+            child: Main(
+              title: 'Pomo Focus',
+              darkModeEnabled: darkModeEnabled,
+              changeDarModeEnabled: changeDarModeEnabled,
+              changeColorSelected: changeColorSelected,
+              colorSelected: colorSelected,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class WindowButtons extends StatelessWidget {
+  final bool isDarkMode;
+
+  const WindowButtons({Key? key, required this.isDarkMode}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        MinimizeWindowButton(
+          colors: WindowButtonColors(
+            iconNormal: isDarkMode ? Colors.black : Colors.white,
+            iconMouseDown: isDarkMode ? Colors.black : Colors.white,
+            iconMouseOver: isDarkMode ? Colors.black : Colors.white,
+            mouseOver: isDarkMode
+                ? Colors.black.withOpacity(0.1)
+                : Colors.white.withOpacity(0.1),
+            mouseDown: isDarkMode
+                ? Colors.black.withOpacity(0.2)
+                : Colors.white.withOpacity(0.2),
+          ),
+        ),
+        MaximizeWindowButton(
+          colors: WindowButtonColors(
+            iconNormal: isDarkMode ? Colors.black : Colors.white,
+            iconMouseDown: isDarkMode ? Colors.black : Colors.white,
+            iconMouseOver: isDarkMode ? Colors.black : Colors.white,
+            mouseOver: isDarkMode
+                ? Colors.black.withOpacity(0.1)
+                : Colors.white.withOpacity(0.1),
+            mouseDown: isDarkMode
+                ? Colors.black.withOpacity(0.2)
+                : Colors.white.withOpacity(0.2),
+          ),
+        ),
+        CloseWindowButton(
+          colors: WindowButtonColors(
+            iconNormal: isDarkMode ? Colors.black : Colors.white,
+            mouseOver: Colors.redAccent,
+            mouseDown: Colors.red,
+          ),
+        ),
+      ],
     );
   }
 }
 
 class Main extends StatefulWidget {
-  const Main({Key? key,
-    required this.title,
-    required this.darkModeEnabled,
-    required this.changeDarModeEnabled,
-    required this.changeColorSelected,
-    required this.colorSelected})
+  const Main(
+      {Key? key,
+      required this.title,
+      required this.darkModeEnabled,
+      required this.changeDarModeEnabled,
+      required this.changeColorSelected,
+      required this.colorSelected})
       : super(key: key);
 
   final String title;
@@ -176,10 +269,8 @@ class _MainState extends State<Main> {
     setState(() {
       _selectedIndex = index;
       pageChanged = true;
-      pageController.animateToPage(
-          index,
-          duration: const Duration(milliseconds: 500),
-          curve: Curves.easeInOut);
+      pageController.animateToPage(index,
+          duration: const Duration(milliseconds: 500), curve: Curves.easeInOut);
     });
   }
 
@@ -199,10 +290,10 @@ class _MainState extends State<Main> {
     return Scaffold(
       bottomNavigationBar: Theme(
         data: Theme.of(context).copyWith(
-          // splashColor: Colors.transparent,
-          // highlightColor: Colors.transparent,
-          // hoverColor: Colors.transparent
-        ),
+            // splashColor: Colors.transparent,
+            // highlightColor: Colors.transparent,
+            // hoverColor: Colors.transparent
+            ),
         child: NavigationBar(
           selectedIndex: _selectedIndex,
           onDestinationSelected: _onItemTapped,
@@ -227,7 +318,8 @@ class _MainState extends State<Main> {
             child: IconButton(
                 onPressed: () {
                   setState(() {
-                    Get.changeThemeMode(Get.isDarkMode ? ThemeMode.light : ThemeMode.dark);
+                    Get.changeThemeMode(
+                        Get.isDarkMode ? ThemeMode.light : ThemeMode.dark);
                     widget.changeDarModeEnabled(Get.isDarkMode);
                   });
                 },
@@ -239,10 +331,10 @@ class _MainState extends State<Main> {
             tooltip: "Show color menu",
             icon: const Icon(Icons.color_lens),
             shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10), side: BorderSide(color: Theme
-                .of(context)
-                .colorScheme
-                .secondaryContainer, width: 0.3)),
+                borderRadius: BorderRadius.circular(10),
+                side: BorderSide(
+                    color: Theme.of(context).colorScheme.secondaryContainer,
+                    width: 0.3)),
             itemBuilder: (context) {
               return List.generate(colorOptions.length, (index) {
                 return PopupMenuItem(
@@ -252,11 +344,15 @@ class _MainState extends State<Main> {
                         Padding(
                           padding: const EdgeInsets.only(left: 10),
                           child: Icon(
-                            index == widget.colorSelected ? Icons.color_lens : Icons.color_lens_outlined,
+                            index == widget.colorSelected
+                                ? Icons.color_lens
+                                : Icons.color_lens_outlined,
                             color: colorOptions[index],
                           ),
                         ),
-                        Padding(padding: const EdgeInsets.only(left: 20), child: Text(colorText[index]))
+                        Padding(
+                            padding: const EdgeInsets.only(left: 20),
+                            child: Text(colorText[index]))
                       ],
                     ));
               });
@@ -267,19 +363,15 @@ class _MainState extends State<Main> {
       ),
       body: SizedBox.expand(
           child: PageView(
-            controller: pageController,
-            allowImplicitScrolling: true,
-            onPageChanged: (index) {
-              setState(() {
-                _selectedIndex = index;
-              });
-            },
-            children: [
-              PomoPage(pageChanged: pageChanged),
-              const SettingsPage()
-            ],
-          )
-      ),
+        controller: pageController,
+        allowImplicitScrolling: true,
+        onPageChanged: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        children: [PomoPage(pageChanged: pageChanged), const SettingsPage()],
+      )),
     );
   }
 }
